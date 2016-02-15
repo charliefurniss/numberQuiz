547101667a8749b766f35d2f3adf3cedda88f2e3
Stewart McCheyne <stewartmccheyne@Stewarts-MacBook-Pro.local>
2016-02-15 14:55:16 +0000

    the answers are showing on the screen

--- js/questionsController.js
+++ js/questionsController.js
@@ -10,45 +10,62 @@ function QuestionsController($http) {
 
 	var self = this;
 
-	self.response = {};
-	self.response2 = {};
-	self.response3 = {}; 
-	// self.number = response.data.number;
-	// self.correctAns = response.data.text
-	// self.secondOpt = response.data.text
-	// self.thirdOpt = response.data.text
+	self.getQuestion= getQuestion;
+	self.number = '';
+	self.correctAnswer = '';
+	self.wrongAnswer1 = '';
+	self.wrongAnswer2 = '';
 
-	self.getQuestion = function() {
+	// setCorrectAnswer();
+	// console.log(self.number);
 
+	self.getQuestion()
+	getWrongAnswer1()
+	getWrongAnswer2()
+	
+
+	function getQuestion() {		
 		$http
 			.get('https://numbersapi.p.mashape.com/random/trivia?fragment=true&json=true', {
 				headers: { 'X-Mashape-Authorization' : '1lM52liN37mshekVR70k9k6SlPrNp13RotojsnLu7QdUmr70jC' }
 			}).then(function(response) {
-				self.responseObject = response.data;
-				console.log(self.responseObject);
-				console.log(self.responseObject.text);
+				//self.responseObject = response.data;
+				self.number = response.data.number;
+				self.correctAnswer = response.data.text
+				console.log(self.number);
 			});
-
+	}
+	
+	function getWrongAnswer1() {
 		$http
 			.get('https://numbersapi.p.mashape.com/random/trivia?fragment=true&json=true', {
 				headers: { 'X-Mashape-Authorization' : '1lM52liN37mshekVR70k9k6SlPrNp13RotojsnLu7QdUmr70jC' }
 			}).then(function(response) {
-				self.responseObject = response.data;
-				console.log(self.responseObject);
-				console.log(self.responseObject.text);
-			});
-
-		$http
-			.get('https://numbersapi.p.mashape.com/random/trivia?fragment=true&json=true', {
-				headers: { 'X-Mashape-Authorization' : '1lM52liN37mshekVR70k9k6SlPrNp13RotojsnLu7QdUmr70jC' }
-			}).then(function(response) {
-				self.responseObject = response.data;
-				console.log(self.responseObject);
-				console.log(self.responseObject.text);
+				self.wrongAnswer1 = response.data.text;
 			});
 	}
 
-	self.getQuestion();
 
+	function getWrongAnswer2() {
+		$http
+			.get('https://numbersapi.p.mashape.com/random/trivia?fragment=true&json=true', {
+				headers: { 'X-Mashape-Authorization' : '1lM52liN37mshekVR70k9k6SlPrNp13RotojsnLu7QdUmr70jC' }
+			}).then(function(response) {
+				self.wrongAnswer2 = response.data.text;
+			});
+	}
+	
+
+	// self.getQuestion(thing, function() {
+
+	// 	console.log("GOT TO LINE 57");
+	// 	console.log(self.number);
+	// 	console.log(self.correctAnswer);
+	// 	console.log(self.wrongAnswer1);
+	// 	console.log(self.wrongAnswer2);
+
+	// });
 
 }
+
+
