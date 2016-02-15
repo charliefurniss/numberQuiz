b887ca64ac221126336954f75b053054d4001b6a
Chiara Buzzi <buzzichiara11@gmail.com>
2016-02-15 11:22:02 +0000

    Created file structure and got controller linked correctly

---
+++ js/questionsController.js
@@ -0,0 +1,11 @@
+angular
+	.module('quizApp')
+	.controller('QuestionsController', QuestionsController)
+
+// QuestionsController.$inject = ['Question'];
+
+function QuestionsController() {
+
+	console.log('Hello!');
+
+}
